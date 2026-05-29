052548a113891f639b56444cde7a1aaa89ce0c8f
ngocbq3 <ngocbq3@fe.edu.vn>
2026-05-29 09:41:16 +0700

    ket noi csdl (models)

---
+++ mvc/Models/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database
+{
+    //Phương thức kết nối đến database
+    public function connect()
+    {
+        //Thông tin về Database
+        $host = "127.0.0.1"; //thông tin server chứa database
+        $db_name = "wd21304_php1"; //Tên Database
+        $username = "root"; //tên tài khoản sử dụng database
+        $password = "";
+        $port = "3306"; //Cổng của database
+
+        //Kết nối đến Database
+        try {
+            $conn = new PDO("mysql:host=$host; dbname=$db_name; charset=utf8; port=$port", $username, $password);
+            echo "Kết nối dữ liệu thành công"; //Câu lệnh test, nếu OK thì xóa đi
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Lỗi kết nối: " . $e->getMessage();
+        }
+    }
+}
--- mvc/index.php
+++ mvc/index.php
@@ -2,11 +2,15 @@
 require_once __DIR__ . "/Controllers/HomeController.php";
 require_once __DIR__ . "/Controllers/ContactController.php";
 
+require_once __DIR__ . "/Models/Database.php";
+
 $act = $_GET['act'] ?? "";
 
 switch ($act) {
     case "":
     case "home":
+        $db = new Database;
+        $db->connect();
         $home = new HomeController;
         $home->index();
         break;
